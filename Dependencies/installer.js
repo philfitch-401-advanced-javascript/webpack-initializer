3c47b8bd8d58a4b72b5b0878654d5069df144525
Phil Fitch <mostreluctantly@gmail.com>
2019-10-28 16:58:57 -0700

    dependencies

---
+++ Dependencies/installer.js
@@ -0,0 +1,24 @@
+const { execSync } = require('child_process');
+
+function installer(path, packages, dev) {
+  console.log('installer initiated');
+  
+  if(dev) {
+    packages.forEach((pkg) => {
+      execSync(`npm i -D ${pkg}`, {
+        cwd: path,
+        stdio: 'inherit'
+      });
+    })
+  } else {
+    packages.forEach((pkg) => {
+      execSync(`npm i ${pkg}`, {
+        cwd: path,
+        stdio: 'inherit'
+      });
+    })
+  }
+  console.log('installer completed');
+};
+
+module.exports = installer;
