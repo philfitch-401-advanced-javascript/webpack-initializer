18e6093288e1ee10ef2fd7c6c390fff00ca9160f
Phil Fitch <mostreluctantly@gmail.com>
2019-10-31 15:40:01 -0700

    installer test passing

--- Dependencies/installer.js
+++ Dependencies/installer.js
@@ -1,18 +1,18 @@
-const { execSync } = require('child_process');
+const childProcess = require('child_process');
 
 function installer(path, packages, dev) {
   console.log('installer initiated');
   
   if(dev) {
     packages.forEach((pkg) => {
-      execSync(`npm i -D ${pkg}`, {
+      childProcess.execSync(`npm i -D ${pkg}`, {
         cwd: path,
         stdio: 'inherit'
       });
     });
   } else {
     packages.forEach((pkg) => {
-      execSync(`npm i ${pkg}`, {
+      childProcess.execSync(`npm i ${pkg}`, {
         cwd: path,
         stdio: 'inherit'
       });
--- tests/installer.test.js
+++ tests/installer.test.js
@@ -6,23 +6,17 @@ childProcess.execSync = jest.fn();
 describe('installer', () => {
   it('installs dependencies', () => {
     const path = 'tests/test-data/some_text.txt';
-    const pkg = ['one', 'two'];
+    const pkg = ['one'];
     const dev = false;
     installer(path, pkg, dev);
-    expect(childProcess.execSync).toHaveBeenCalledWith((`npm i ${pkg[0]}`, {
-      cwd: path,
-      stdio: 'inherit'
-    }));
+    expect(childProcess.execSync).toHaveBeenCalledWith('npm i one', { 'cwd': 'tests/test-data/some_text.txt', 'stdio': 'inherit' });
   });
 
   it('installs dev dependencies', () => {
     const path = 'tests/test-data/some_text.txt';
-    const pkg = ['one', 'two'];
+    const pkg = ['one'];
     const dev = true;
     installer(path, pkg, dev);
-    expect(childProcess.execSync).toHaveBeenCalledWith((`npm i -D ${pkg}`, {
-      cwd: path,
-      stdio: 'inherit'
-    }));
+    expect(childProcess.execSync).toHaveBeenCalledWith('npm i -D one', { 'cwd': 'tests/test-data/some_text.txt', 'stdio': 'inherit' });
   });
 });
